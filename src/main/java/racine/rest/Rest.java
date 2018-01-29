6066b51bbad3a05e419f765544777151517dda46
ANDRESebastien <andreseba_3@hotmail.com>
2018-01-29 14:29:44 +0100

    début Spring rest OK web Ko

---
+++ src/main/java/racine/rest/Rest.java
@@ -0,0 +1,20 @@
+package racine.rest;
+
+public class Rest {
+
+	private final long id;
+	private final String content;
+
+	public Rest(long id, String content) {
+		this.id = id;
+		this.content = content;
+	}
+
+	public long getId() {
+		return id;
+	}
+
+	public String getContent() {
+		return content;
+	}
+}
